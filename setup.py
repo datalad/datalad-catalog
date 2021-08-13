5836c0d349f53e5794cf52b4aafaad2765fb6d90
Stephan Heunis <s.heunis@fz-juelich.de>
2021-08-13 15:39:41 +0200

    update setup

--- setup.py
+++ setup.py
@@ -10,19 +10,19 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="datalad_catalog",
-    version="1.0.0",
+    version="0.1.0",
     description="Generate a web-based data browser from a DataLad dataset's metadata",
     long_description=README,
     long_description_content_type="text/markdown",
     url="https://github.com/jsheunis/data-browser-from-metadata",
     author="DataLad developers",
     license="MIT",
-    packages=["datalad-catalog"],
+    packages=["datalad_catalog"],
     include_package_data=True,
     # install_requires=["feedparser", "html2text"],
     entry_points={
         "console_scripts": [
-            'datalad-catalog = datalad_catalog.webui_generate:run_cmd',
+            'datalad_catalog = datalad_catalog.webui_generate:run_cmd',
             ]
     },
 )
