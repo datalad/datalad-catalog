844aff4c9a4d387f3594f0c15df7baefd9b3eaa5
Stephan Heunis <s.heunis@fz-juelich.de>
2023-02-06 13:44:39 +0100

    fix ModuleNotFoundError because of upstream metadata updates

--- datalad_catalog/extractors/datacite_gin.py
+++ datalad_catalog/extractors/datacite_gin.py
@@ -18,11 +18,14 @@ from datalad_metalad.extractors.base import (
     ExtractorResult,
 )
 from datalad.log import log_progress
-from datalad.metadata.definitions import vocabulary_id
 from datalad.utils import assure_unicode
 
 
 lgr = logging.getLogger("datalad.metadata.extractors.datacite_gin")
+
+# From metalad legacy; TODO: remove/replace when it becomes useful
+# identifiers that defines an ontology as a whole
+vocabulary_id = 'http://purl.org/dc/dcam/VocabularyEncodingScheme'
 
 datacite_context = {
     "@id": "https://gin.g-node.org/G-Node/Info/src/master/datacite.yml",
