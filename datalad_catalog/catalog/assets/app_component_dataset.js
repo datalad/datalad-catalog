b9c14c13f56430f84bd54bac9bef3b6509d06212
Stephan Heunis <s.heunis@fz-juelich.de>
2023-04-19 10:58:45 +0200

    fix GIN url stripping and sunstring replacing

--- datalad_catalog/catalog/assets/app_component_dataset.js
+++ datalad_catalog/catalog/assets/app_component_dataset.js
@@ -142,7 +142,10 @@ const datasetView = () =>
                   if (dataset.url[i].toLowerCase().indexOf("gin.g-node") >= 0) {
                     disp_dataset.is_gin = true;
                     disp_dataset.url = dataset.url[i];
+                    disp_dataset.url = disp_dataset.url.replace('ssh://', '');
                     disp_dataset.url = disp_dataset.url.replace('git@gin.g-node.org:', 'https://gin.g-node.org');
+                    disp_dataset.url = disp_dataset.url.replace('git@gin.g-node.org', 'https://gin.g-node.org');
+                    disp_dataset.url = disp_dataset.url.replace('.git', '');
                   }
                 }
                 if (!disp_dataset.url) {
