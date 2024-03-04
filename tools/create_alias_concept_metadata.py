ab2fbeabb51696a534daeb0f43c2e969d8e1a1b7
Stephan Heunis <s.heunis@fz-juelich.de>
2024-03-04 23:50:28 +0100

    update script to allow adding dataset aliases to metadata from tsv file

--- tools/create_alias_concept_metadata.py
+++ tools/create_alias_concept_metadata.py
@@ -1,33 +1,46 @@
 """
 With this script you can do the following for an existing catalog:
-- create aliases for datasets from a tsv file (TODO)
+- create aliases for datasets from a tsv file
 - create alias and concept id metadata files for all datasets
 """
 from argparse import ArgumentParser
+import csv
 import json
+from pathlib import Path
+from datalad.api import  (
+    catalog_add,
+)
 from datalad_catalog.constraints import EnsureWebCatalog
 from datalad_catalog.utils import md5hash
+from datalad_catalog.schema_utils import get_metadata_item
 
-if __name__ == "__main__":
-    parser = ArgumentParser()
-    parser.add_argument(
-        "--catalog",
-        type=str,
-        help="Path to the catalog",
-    )
-    parser.add_argument(
-        "--aliases",
-        type=str,
-        help="Path to tsv file with dataset ids and aliases",
-    )
-    args = parser.parse_args()
-    # Ensure is a catalog
-    catalog = EnsureWebCatalog()(args.catalog)
+
+def add_aliases(alias_path, catalog):
+
+    alias_path = Path(alias_path)
+    with alias_path.open(newline='') as tsvfile:
+        reader = csv.DictReader(tsvfile, delimiter='\t')
+        for i, row in enumerate(reader):
+            meta_item = get_metadata_item(
+                item_type="dataset",
+                dataset_id=row["dataset_id"],
+                dataset_version=row["dataset_version"],
+                source_name="automated_alias_entry",
+                source_version="0.1.0",
+                required_only=True,
+                exclude_keys=[],
+            )
+            meta_item["alias"] = row["alias"]
+            catalog_add(
+                catalog=catalog,
+                metadata=json.dumps(meta_item),
+            )
+
+def create_metadata_files(catalog):
     # Get report
     report = catalog.get_catalog_report()
-
     # Write metadata for all datasets
-    # assumes that datasets have aliases set
+    # - assumes that datasets have aliases set
     for d in report.get("datasets", []):
         # Get latest version
         current_ds_versions = [
@@ -38,14 +51,12 @@ if __name__ == "__main__":
             key=lambda d: d.get("updated_at"),
             reverse=True,
         )
-
         # Dict for metadata
         redirect_dict = {
             "type": "redirect",
             "dataset_id": d,
             "dataset_version": sorted_versions[0]["dataset_version"],
         }
-
         # Create id concept metadata
         dataset_concept_path = (
             catalog.metadata_path / d / md5hash(d)
@@ -63,3 +74,27 @@ if __name__ == "__main__":
         dataset_alias_path.parent.mkdir(parents=True, exist_ok=True)
         with open(dataset_alias_path, "w") as f:
             json.dump(redirect_dict, f)
+
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--catalog",
+        type=str,
+        help="Path to the catalog",
+    )
+    parser.add_argument(
+        "--aliases",
+        type=str,
+        help="Path to tsv file with dataset ids and aliases",
+    )
+    args = parser.parse_args()
+    # Ensure is a catalog
+    catalog = EnsureWebCatalog()(args.catalog)
+
+    # If aliases are provided, first set them in metadata
+    if (args.aliases):
+        add_aliases(args.aliases, catalog)
+
+    create_metadata_files(catalog)
