e103e99e90663c076d4a7ecd00ad700842c9bdbf
Michał Szczepanik <m.szczepanik@fz-juelich.de>
2024-05-06 11:51:04 +0200

    Fix openWithBinder handling of custom notebook URLs
    After the ability to modify content url, repo name, or notebook name
    was introduced in 5ac68d7, these can no longer be const, and need to
    be var.
    
    Otherwise, clicking the "Explore with Binder" button would log a
    "TypeError: assignment to constant variable" in the console and do
    nothing else if custom notebook information was present.

--- datalad_catalog/catalog/assets/app_component_dataset.js
+++ datalad_catalog/catalog/assets/app_component_dataset.js
@@ -758,9 +758,9 @@ const datasetView = () =>
           openWithBinder(dataset_url, current_dataset) {
             const environment_url =
               "https://mybinder.org/v2/gh/datalad/datalad-binder/main";
-            const content_url = "https://github.com/jsheunis/datalad-notebooks";
-            const content_repo_name = "datalad-notebooks";
-            const notebook_name = "download_data_with_datalad_python.ipynb";
+            var content_url = "https://github.com/jsheunis/datalad-notebooks";
+            var content_repo_name = "datalad-notebooks";
+            var notebook_name = "download_data_with_datalad_python.ipynb";
             if (current_dataset.hasOwnProperty("notebooks") && current_dataset.notebooks.length > 0) {
               // until including the functionality to select from multiple notebooks in a dropdown, just select the first one
               notebook = current_dataset.notebooks[0]
